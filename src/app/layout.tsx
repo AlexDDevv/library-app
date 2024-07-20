a9787b3f44e2a3f7ef0b1476fd3a0f925de4d3c9
Alexis <AlexDdevs@gmail.com>
2024-07-20 22:09:54 +0200

    💄 Remove max-with and add padding bottom

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
 }>) {
     return (
         <html lang="en">
-            <body className="bg-background max-w-[1440px] mx-auto my-0 py-5">
+            <body className="bg-background py-[50px]">
                 <ClientProvider>
                     {children}
                 </ClientProvider>
