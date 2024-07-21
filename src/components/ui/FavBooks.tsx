fae6b29e6a5fcde1813e719a8ceeeaffc31fa505
Alexis <AlexDdevs@gmail.com>
2024-07-21 18:07:06 +0200

    🎨 Create a component for favorite books

---
+++ src/components/ui/FavBooks.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function FavBooks() {
+    return (
+        <div className='bg-card rounded-[10px] w-[250px] h-[430px] py-6 px-10 text-center shadow-[0_5px_5px_2px_rgb(0,0,0,0.25)] flex items-center justify-center'>
+            <p className='font-bodyFont font-medium text-sm  text-card-foreground'>Vous n'avez pas encore ajouté de livre en favoris.</p>
+        </div>
+    )
+}
