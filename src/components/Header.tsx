1b3299a5cef57369cbf0724a49116d1bebd12765
Alexis <AlexDdevs@gmail.com>
2024-08-21 14:05:18 +0200

    🎨 Use new data for nav link

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,20 +1,20 @@
 import React from 'react'
-import Link from 'next/link'
 import Logo from './Logo'
+import data from "../data/data.json"
+import Link from 'next/link'
 import NavBarBtn from './ui/NavBarBtn'
 
 export default function Header() {
     return (
         <header className='bg-card flex items-center justify-between rounded-xl px-5 py-4 border border-border max-w-[1440px] mx-auto mt-0 mb-[100px]'>
             <Logo />
-            <nav>
+            <nav className='flex items-center gap-[50px]'>
                 <ul className='flex items-center justify-center gap-[50px]'>
-                    <li className='transition-transform hover:scale-110'>
-                        <Link href="/recherche" className='font-bodyFont text-card-foreground text-lg font-medium'>Recherche</Link>
-                    </li>
-                    <li className='transition-transform hover:scale-110'>
-                        <Link href="/bibliotheque" className='font-bodyFont text-card-foreground text-lg font-medium'>Bibliothèque</Link>
-                    </li>
+                    {data.navLink.map((link, i) => (
+                        <li className='transition-transform hover:scale-110' key={i}>
+                            <Link href={link.link} className='font-bodyFont text-card-foreground text-lg font-medium'>{link.content}</Link>
+                        </li>
+                    ))}
                 </ul>
             </nav>
             <NavBarBtn />
